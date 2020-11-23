c65df659dba057103c37d05567e9930e3ac83e19
HuanZ <huanzhangrain@gmail.com>
2020-11-23 18:10:48 +0800

    002

---
+++ Basic/005.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // assign a list of 2 integer variables [1,1]
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while(fibonacciNumbers.Count < 20)
+            {
+                // -1 to get the last element of array because it start with zero
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+
+        }
+    }
+}
